064eb7fee1fb5ba12a28c8bedfe3e3ff540c0fbc
1n13s <ivonneangm@gmail.com>
2024-06-12 11:18:06 -0600

    Comment jwt_create

--- CODE/ApiTaskManager/auth/jwt_create.py
+++ CODE/ApiTaskManager/auth/jwt_create.py
@@ -1,6 +1,7 @@
 from jwt import encode, decode, exceptions
 from datetime import datetime, timedelta
 from fastapi.responses import JSONResponse
+from typing import Dict
 from ApiTaskManager.core.user_manager import UserManager
 from OpenSSL import rand
 
@@ -8,14 +9,35 @@ from OpenSSL import rand
 SECRET='ge8ZXhERUrmTic6rcHYMKKOc75qbNdiy'
 ALGORITHM = 'HS256'
 
-def expire_date(minutes: int):
+def expire_date() -> datetime:
+    """Obtains the expire date
+
+    Returns:
+        datetime: The expire date with one day
+    """
     date = datetime.now()
-    return date + timedelta(days=minutes)
+    return date + timedelta(days=1)
 
-def write_token(data: dict):
-    return encode(payload={**data, "exp": expire_date(30)}, key=SECRET, algorithm=ALGORITHM)
+def write_token(login_info: dict) -> str:
+    """Creates the token
 
-def verify_token(token):
+    Args:
+        login_info (dict): The info of login 
+
+    Returns:
+        str: The token with the info
+    """
+    return encode(payload={**login_info, "exp": expire_date()}, key=SECRET, algorithm=ALGORITHM)
+
+def verify_token(token) -> Dict[str, str]|bool:
+    """Verifies a token given
+
+    Args:
+        token (any): The token with the login info
+
+    Returns:
+        Dict[str, str]|bool: The validation of the decoding or token status
+    """
     try: 
         decode(token, key=SECRET, algorithms=ALGORITHM)
         return True
@@ -26,7 +48,14 @@ def verify_token(token):
     except Exception as e:
         return JSONResponse(content={"message": f"The auth validation has failed {e}"}, status_code=500)
     
+def read_token(token) -> str:
+    """Reads and decodes the token
 
-def read_token(token):
+    Args:
+        token (any): The token with the login info
+
+    Returns:
+        str: The token decoe
+    """
     return decode(token, key=SECRET, algorithms=ALGORITHM)
 
